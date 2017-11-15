943863b5c685948d3fea56cc2fd2ee8821303321
diegogeandafe <diegogeandafe@gmail.com>
2017-11-15 19:59:04 -0200

    Criando método listarPizzas() e salvarPizza() em PizzaController() e adicionando listeners

--- src/main/java/br/com/diegogeandafe/pizzaria/Aplicacao.java
+++ src/main/java/br/com/diegogeandafe/pizzaria/Aplicacao.java
@@ -1,9 +1,14 @@
 package br.com.diegogeandafe.pizzaria;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -23,6 +28,22 @@ public class Aplicacao implements WebApplicationInitializer {
 		appServlet.addMapping("/app/*");
 		
 		servletContext.addListener(new ContextLoaderListener(webApplicationContext));
-	}	
+		
+		FilterRegistration.Dynamic filter = servletContext.addFilter("openEntityManagerFilter", buildOpenEntityManagerFilter());
+		filter.addMappingForServletNames(getDispatcherTypes(), false, "/app/*");
+	}
+	
+	//Faz com que toda as requisições que o usuário envia seja aberto uma nova sessão de banco de dados
+	//utilziando so o Transacional vai abrir uma transação porem a sessão com o banco de dados vai ficar
+	//aberto desde o inicio da requisição ate o final
+	private OpenEntityManagerInViewFilter buildOpenEntityManagerFilter() {
+		OpenEntityManagerInViewFilter openEntityManagerInViewFilter = new OpenEntityManagerInViewFilter();
+		openEntityManagerInViewFilter.setEntityManagerFactoryBeanName("entityManagerFactory");
+		return openEntityManagerInViewFilter;
+	}
+	
+	private EnumSet<DispatcherType> getDispatcherTypes() {
+		return EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE, DispatcherType.ASYNC);
+	}
 	
 }
